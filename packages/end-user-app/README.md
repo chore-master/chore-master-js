d90920972ad306a31a5df1f342d4c5f6bf40dacc
gocreating <gocreating@gmail.com>
2024-05-05 15:52:24 +0800

    Update

---
+++ packages/end-user-app/README.md
@@ -0,0 +1,6 @@
+# End User App
+
+```sh
+yarn workspace @app/end-user-app add <package-name>
+yarn workspace @app/end-user-app dev
+```
---
+++ packages/end-user-app/src/app/abc/page.tsx
@@ -0,0 +1,9 @@
+import SampleComponent from '@module/utils/SampleComponent'
+
+export default function Abc() {
+  return (
+    <div>
+      <SampleComponent>xx</SampleComponent>
+    </div>
+  )
+}
