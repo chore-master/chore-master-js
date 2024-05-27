70c2e8c2b4d9c3a51ad8218617de0951ad6fe7ad
gocreating <gocreating@gmail.com>
2024-05-27 22:59:26 +0800

    Update

--- packages/end-user-app/src/app/(auth)/login/page.tsx
+++ packages/end-user-app/src/app/(auth)/login/page.tsx
@@ -1,24 +1,31 @@
-'use client'
+// 'use client'
 
-import apiAgent from '@/utils/apiAgent'
+// import apiAgent from '@/utils/apiAgent'
+import getConfig from '@/utils/config'
 import Link from 'next/link'
-import React from 'react'
 
 export default function Page() {
-  React.useEffect(() => {
-    apiAgent.get(`/openapi.json`, {
-      onFail: (_status: any, data: any) => {
-        console.log(data)
-        // console.error(data)
-      },
-      onSuccess: async (data: any) => {
-        try {
-          console.log(data)
-        } catch (e) {
-          console.error(e)
-        }
-      },
-    } as any)
-  })
-  return <Link href="/login">Login with Google</Link>
+  const { HOST, IAM_API_HOST } = getConfig()
+  const nextURI = encodeURI(`${HOST}/`)
+
+  // React.useEffect(() => {
+  //   apiAgent.get(`/openapi.json`, {
+  //     onFail: (_status: any, data: any) => {
+  //       console.log(data)
+  //       // console.error(data)
+  //     },
+  //     onSuccess: async (data: any) => {
+  //       try {
+  //         console.log(data)
+  //       } catch (e) {
+  //         console.error(e)
+  //       }
+  //     },
+  //   } as any)
+  // })
+  return (
+    <Link href={`${IAM_API_HOST}/api/auth/google/authorize?nextURI=${nextURI}`}>
+      Login with Google
+    </Link>
+  )
 }
