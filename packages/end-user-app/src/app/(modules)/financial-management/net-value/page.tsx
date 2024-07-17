a61fff1afaa931a5c4b7441ce7ecddf9af3bfd57
gocreating <gocreating@gmail.com>
2024-07-18 00:52:17 +0800

    Update

--- packages/end-user-app/src/app/(modules)/financial-management/net-value/page.tsx
+++ packages/end-user-app/src/app/(modules)/financial-management/net-value/page.tsx
@@ -3,8 +3,26 @@
 import { ModuleDataGrid } from '@/components/ModuleDataGrid'
 import ModuleFunction, { ModuleFunctionBody } from '@/components/ModuleFunction'
 import { useEntity } from '@/utils/entity'
-import { GridColDef, GridRowModesModel, GridRowsProp } from '@mui/x-data-grid'
+import CancelIcon from '@mui/icons-material/Close'
+import DeleteIcon from '@mui/icons-material/DeleteOutlined'
+import EditIcon from '@mui/icons-material/Edit'
+import SaveIcon from '@mui/icons-material/Save'
+import {
+  GridActionsCellItem,
+  GridColDef,
+  GridRenderEditCellParams,
+  GridRowId,
+  GridRowModel,
+  GridRowModes,
+  GridRowModesModel,
+  GridRowsProp,
+} from '@mui/x-data-grid'
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+function CustomEditComponent(props: GridRenderEditCellParams) {
+  return <input type="text" />
+}
 
 export default function Page() {
   const account = useEntity<GridRowsProp>({
@@ -32,7 +50,6 @@ export default function Page() {
           accountReferenceToAccountMap?.[netValue.account_reference]
         const settlementAsset =
           assetReferenceToAssetMap?.[netValue.settlement_asset_reference]
-
         return {
           ...netValue,
           account_name: account?.name,
@@ -41,6 +58,56 @@ export default function Page() {
       })
     )
   }, [account.list, asset.list, netValue.list])
+
+  const getNewNetValueRow = () => {
+    return {
+      isNew: true,
+      reference: uuidv4(),
+      account_reference: account.list[0]?.reference,
+      settlement_asset_reference: asset.list[0]?.reference,
+    }
+  }
+
+  const handleEditNetValueClick = (reference: GridRowId) => () => {
+    setNetValueRowModesModel({
+      ...netValueRowModesModel,
+      [reference]: { mode: GridRowModes.Edit },
+    })
+  }
+
+  const handleSaveNetValueClick = (reference: GridRowId) => () => {
+    setNetValueRowModesModel({
+      ...netValueRowModesModel,
+      [reference]: { mode: GridRowModes.View },
+    })
+  }
+
+  const handleDeleteNetValueClick = (reference: GridRowId) => async () => {
+    await netValue.deleteByReference(reference)
+  }
+
+  const handleCancelEditNetValueClick = (reference: GridRowId) => () => {
+    setNetValueRowModesModel({
+      ...netValueRowModesModel,
+      [reference]: { mode: GridRowModes.View, ignoreModifications: true },
+    })
+    const editedRow = netValue.list.find((row) => row.reference === reference)
+    if (editedRow!.isNew) {
+      netValue.setList(
+        netValue.list.filter((row) => row.reference !== reference)
+      )
+    }
+  }
+
+  const handleUpsertNetValueRow = async ({
+    isNew,
+    ...upsertedRow
+  }: GridRowModel) => {
+    return await netValue.upsertByReference({
+      isNew,
+      upsertedEntity: upsertedRow,
+    })
+  }
 
   const netValueColumns: GridColDef[] = [
     {
@@ -59,6 +126,9 @@ export default function Page() {
       field: 'account_name',
       type: 'string',
       headerName: '帳戶名稱',
+      renderEditCell: (params: GridRenderEditCellParams) => (
+        <CustomEditComponent {...params} />
+      ),
     },
     {
       field: 'amount',
@@ -88,7 +158,45 @@ export default function Page() {
       type: 'actions',
       cellClassName: 'actions',
       getActions: ({ id }) => {
-        return []
+        const isInEditMode =
+          netValueRowModesModel[id]?.mode === GridRowModes.Edit
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              key="save"
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: 'primary.main',
+              }}
+              onClick={handleSaveNetValueClick(id)}
+            />,
+            <GridActionsCellItem
+              key="cancel"
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelEditNetValueClick(id)}
+              color="inherit"
+            />,
+          ]
+        }
+        return [
+          <GridActionsCellItem
+            key="edit"
+            icon={<EditIcon />}
+            label="Edit"
+            onClick={handleEditNetValueClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            key="delete"
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteNetValueClick(id)}
+            color="inherit"
+          />,
+        ]
       },
     },
   ]
@@ -102,7 +210,9 @@ export default function Page() {
             columns={netValueColumns}
             rowModesModel={netValueRowModesModel}
             onRowModesModelChange={setNetValueRowModesModel}
-            setRows={netValue.setList}
+            getNewRow={getNewNetValueRow}
+            setRows={setNetValueRows}
+            processRowUpdate={handleUpsertNetValueRow}
             loading={netValue.isLoading}
             getRowId={(row) => row.reference}
             columnVisibilityModel={{
