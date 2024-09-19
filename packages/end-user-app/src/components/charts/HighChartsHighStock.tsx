a90af2da2375149a74664010ddb70f291ed6e933
CP Weng <cp.weng@tkspring.com>
2024-09-19 18:16:59 +0800

    Update

--- packages/end-user-app/src/components/charts/HighChartsHighStock.tsx
+++ packages/end-user-app/src/components/charts/HighChartsHighStock.tsx
@@ -23,6 +23,54 @@ export default function HighChartsHighStock({
 }) {
   const chartComponentRef = React.useRef<HighchartsReact.RefObject>(null)
   const options: Highcharts.Options = {
+    xAxis: {
+      type: 'datetime',
+      labels: {
+        format: '{value:%Y-%m-%d}',
+        rotation: -45,
+      },
+    },
+    navigator: {
+      maskInside: false,
+      maskFill: 'rgba(102,133,194,0.3)',
+      xAxis: {
+        labels: {
+          format: '{value:%Y-%m}',
+        },
+      },
+    },
+    tooltip: {
+      // outside: true,
+      // split: false,
+      // shared: true,
+      // useHTML: true,
+      headerFormat: '{point.x:%Y-%m-%d %H:%M:%S.%L}',
+      // pointFormat:
+      //   '<p style="font-size:12px;margin:0px;padding-top:1px;padding-bottom:1px;color:{series.color};">{series.name} <strong>{point.y}</strong></p>',
+      // valueDecimals: 2,
+      // backgroundColor: 'black',
+      borderWidth: 1,
+      // style: {
+      //   width: 150,
+      //   padding: '0px',
+      // },
+      // shadow: false,
+
+      // xDateFormat: '%Y-%m-%d, %A',
+      // headerFormat:
+      //   '<span style="font-size: 10px">{point.key:%Y-%m-%d}</span><br/>',
+      // outside: true,
+      //   dateTimeLabelFormats: {
+      //     year: '%Y',
+      //     month: '%Y-%m',
+      //     week: '%Y-%m-%d',
+      //     day: '%Y-%m-%d',
+      //     hour: '%Y-%m-%d %H:%M',
+      //     minute: '%Y-%m-%d %H:%M',
+      //     second: '%Y-%m-%d %H:%M:%S',
+      //     millisecond: '%Y-%m-%d %H:%M:%S.%L',
+      //   },
+    },
     rangeSelector: {
       selected: 3, // Pre-selects one of the range buttons
       buttons: [
@@ -46,6 +94,7 @@ export default function HighChartsHighStock({
           text: 'All',
         },
       ],
+      inputDateFormat: '%Y-%m-%d',
     },
     plotOptions: {
       column: {
