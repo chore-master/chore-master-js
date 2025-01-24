1ae8fa76707ce3d8e90a58f99dc492f16dde1711
CP Weng <cp.weng@tkspring.com>
2025-01-24 16:27:34 +0800

    Update

--- packages/end-user-app/src/app/(modules)/admin/layout.tsx
+++ packages/end-user-app/src/app/(modules)/admin/layout.tsx
@@ -18,34 +18,7 @@ export default function Layout({
             {
               type: 'link',
               title: '資料庫',
-              href: '/iam/core/database',
-              selectedWhenPartiallyMatched: true,
-            },
-          ],
-        },
-        {
-          type: 'divider',
-        },
-        {
-          type: 'header',
-          title: '服務整合',
-          navigations: [
-            {
-              type: 'link',
-              title: 'Google',
-              href: '/iam/integrations/google',
-              selectedWhenPartiallyMatched: true,
-            },
-            {
-              type: 'link',
-              title: '永豐',
-              href: '/iam/integrations/sinotrade',
-              selectedWhenPartiallyMatched: true,
-            },
-            {
-              type: 'link',
-              title: 'OKX',
-              href: '/iam/integrations/okxtrade',
+              href: '/admin/core/database',
               selectedWhenPartiallyMatched: true,
             },
           ],
